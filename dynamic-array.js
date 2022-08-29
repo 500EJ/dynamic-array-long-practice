1b5bf7f98a8ec012659071fc0af41608d4dbc2af
Ethan Julius <56324629+500EJ@users.noreply.github.com>
2022-08-29 12:09:47 -0400

    Implement DynamicArray

--- dynamic-array.js
+++ dynamic-array.js
@@ -1,34 +1,60 @@
 class DynamicArray {
   constructor(defaultSize = 4) {
-    // Your code here
+    this.length = 0;
+    this.capacity = defaultSize;
+    this.data = new Array(defaultSize);
   }
 
   read(index) {
-    // Your code here
+    return this.data[index];
   }
 
   push(val) {
-    // Your code here
+    if (this.length + 1 > this.capacity) this.resize();
+    this.data[this.length] = val;
+    this.length++;
   }
 
   pop() {
-    // Your code here
+    this.length--;
+    const lastElement = this.data[this.length];
+    this.data[this.length] = undefined;
+    return lastElement;
   }
 
   shift() {
-    // Your code here
+    const firstElement = this.data[0];
+    for (let i = 1; i < this.length; i++) {
+      this.data[i - 1] = this.data[i];
+    }
+    this.length--;
+    this.data[this.length] = undefined;
+    return firstElement;
   }
 
   unshift(val) {
-    // Your code here
+    if (this.length + 1 > this.capacity) this.resize();
+    for (let i = this.length; i >= 1; i--) {
+      this.data[i] = this.data[i - 1];
+    }
+    this.data[0] = val;
+    this.length++;
   }
 
   indexOf(val) {
-    // Your code here
+    for (let i = 0; i < this.length; i++) {
+      if (this.data[i] === val) return i;
+    }
+    return -1;
   }
 
   resize() {
-    // Your code here
+    const newArr = new Array(this.capacity * 2);
+    for (let i = 0; i < this.data.length; i++) {
+      newArr[i] = this.data[i];
+    }
+    this.capacity *= 2;
+    this.data = newArr;
   }
 }
 
